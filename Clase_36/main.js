9de19ba799a69637509715140747d10ae76ef6d5
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-03-07 13:14:08 -0300

    Clase_36

---
+++ Clase_36/main.js
@@ -0,0 +1,53 @@
+import express from 'express'
+import twilio from 'twilio'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const app = express()
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+
+const client = twilio(accountSid, authToken)
+
+
+// const numbers = ['+5491153267449', '+5492615909801']
+
+try {
+    let messasge = ''
+
+    for (const number of numbers) {
+        messasge = await client.messages.create({
+            from: 'whatsapp:+14155238886',
+            to: `whatsapp:${number}`,
+            body: 'Todo funciona bien con Tweilio y NodeJS! - clase_36',
+            mediaUrl: ['https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?w=800']
+        })
+        console.log(messasge);
+    }
+} catch (error) {
+    console.log(error);
+}
+
+
+const options = {
+    // cuerpo del mensaje
+    body: "Hola soy un WSP desde nodeJS",
+    mediaUrl: ["https://pbs.twimg.com/media/D4uQCqNW4AA4s0I.jpg"],
+    from: "whatsapp:+14155238886",
+    to: "whatsapp:+5491153267449",
+};
+
+app.post('/twilio-coder', async (req, res) => {
+    try {
+        const message = await client.messages.create(options);
+        res.send({ data: message })
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+app.listen(8080, () => {
+    console.log('Server ok');
+})
